576acd56b7c125901ed03b94374dfcc82ab351aa
andy3520 <andy.annguyen3520@gmail.com>
2018-12-13 12:27:05 +0700

    deploy now 1

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -8,7 +8,7 @@ exports.getUsers = function(req, res, next) {
       if(req.session.user.type == 0){
         var params = {
           TableName: "Users"
-      } 
+      }
       docClient.scan(params, function(err, data){
           if (err) {
               console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
